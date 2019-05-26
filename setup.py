1c4708f47a4d30583d784d3bac8cd4a6058b46ac
prakash besra <psbesra@gmail.com>
2019-05-26 14:02:57 +0530

    license removed

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(
       download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
       keywords = ['date', 'time', 'datetime', 'dateformat', 'timeformat', 'datetimeformat'],   # Keywords that define your package best
       
-      # I get to this in a second
-      install_requires=[],
       classifiers=[
               'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
               'Intended Audience :: Developers',      # Define that your audience are developers
