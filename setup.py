b5987c6a4d6191767479041e0d4e4e4bdab6c8ee
prakash besra <psbesra@gmail.com>
2019-05-26 17:31:29 +0530

    download_url updated

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
       name = 'datetime_format',         # How you named your package folder (MyLib)
       packages = ['datetime_format'],   # Chose the same as "name"
-      version = '1.0.3',      # Start with a small number and increase it with every change you make
+      version = '1.0.4',      # Start with a small number and increase it with every change you make
       license=None,        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
       description = 'It converts dateime string into another datetime string with formatting done on it by user',   # Give a short description about your library
       author = 'Prakash Besra',                   # Type in your name
       author_email = 'psbesra@gmail.com',      # Type in your E-Mail
       url = 'https://github.com/pbesra',   # Provide either the link to your github or to your website
-      download_url = 'https://github.com/pbesra/datetime_format/archive/v1.0.3.tar.gz',    
+      download_url = 'https://github.com/pbesra/datetime_format/archive/v1.0.4.tar.gz',    
       keywords = ['date', 'time', 'datetime', 'dateformat', 'timeformat', 'datetimeformat'],   # Keywords that define your package best
       
       classifiers=[
